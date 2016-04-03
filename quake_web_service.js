f2637a8740451f5eccb7f94bdb9315ac543d48a1
Brandon <bran.cham@gmail.com>
2016-04-02 22:21:02 -0700

    Added web listener for github

---
+++ quake_web_service.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var app = express();
+var CryptoJS = require('crypto-js');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+
+var key = process.env[2]; //get the key from command line, why not
+
+var quake_response = {
+    status: "success",
+    details: "Code pulled in, compiled, copied progs.dat, changed level!"
+};
+
+app.post('/deploy-quake-dev', function (req, res) {
+    var sent_secret = req.get('X-Hub-Signature');
+    var my_generated_secret = "sha1=" + CryptoJS.HmacSHA1( JSON.stringify(req.body), key);
+
+    if( my_generated_secret != sent_secret ) {
+        res.status(403);
+        return res.json({ status: "failed", reason: "Bad login" });
+    }
+    var exec = require('child_process').exec;
+    var child = exec('/home/ubuntu/deploy-quake-dev.sh',
+        function(error, stdout, stderr) {
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+            if (error !== null) {
+                res.status(500).json({ error: 'Failed to deploy runequake code' });
+            }
+            return res.json(quake_response);
+    });
+});
+
+app.get('/deploy-quake-prod', function (req, res) {
+    var guid = req.query.guid;
+    if( key != guid ) {
+        res.status(403);
+        return res.json({ status: "failed", reason: "Bad login" });
+    }
+    quake_response = {
+        status: "success",
+        details: "Copied progs.dat to /runequake folder, changed level!"
+    };
+
+    var exec = require('child_process').exec;
+    var child = exec('/home/ubuntu/deploy-quake-prod.sh',
+        function(error, stdout, stderr) {
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+            if (error !== null) {
+                res.status(500).json({ error: 'Failed to deploy runequake code' });
+            }
+            return res.json(quake_response);
+    });
+});
+
+app.listen(8530, function () {
+    console.log('Quake server applet listening on port 8530!');
+});
