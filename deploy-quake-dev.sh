17e38b45a35a33d7f49aa5d5d3ac7147745522a4
Brandon <bran.cham@gmail.com>
2016-04-06 21:21:12 -0700

    Don't change level if compile failed..

--- deploy-quake-dev.sh
+++ deploy-quake-dev.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 cd /home/ubuntu/quake/runequake-dev
+rm src/map-auto.qc
+git checkout src/*.qc
 git pull
 cd src
 make
+$rc=$?
+if[ $rc != 0 ]; then
+    echo "RuneQuake compile FAILED..exiting"
+    exit
+fi
 cd ../
 cp progs.dat ../
 screen -S dev_server -p 0 -X stuff "changelevel start$(printf \\r)"
