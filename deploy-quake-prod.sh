3bff46c9dadc1b3728181302081713039a3eaecf
Brandon <bran.cham@gmail.com>
2016-05-26 18:44:26 -0700

    Adding autoexec.cfg step to deploy process for prod.

--- deploy-quake-prod.sh
+++ deploy-quake-prod.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+cd /home/ubuntu/
+
+#copy in from git checkout
+cp /home/ubuntu/quake/runequake-dev/autoexec-prod.cfg /home/ubuntu/quake/runequake/autoexec.cfg
+
+#merge in password from conf file
+cat rq_rcon.conf | xargs -I '{}' sed -i 's/{rcon_password}/{}/g' /home/ubuntu/quake/runequake/autoexec.cfg
+
 cp /home/ubuntu/quake/runequake-dev/progs.dat /home/ubuntu/runequake/progs.dat
